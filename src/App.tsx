b3a1931632fcd54974f1ca832b5a8692c035456b
bigwave-cho <hooyaa93@gmail.com>
2023-03-01 22:52:07 +0900

    framer-motion : drag(dragConstraints ~ Elastic)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
+import { useRef } from 'react';
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
@@ -7,41 +8,58 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-// ## How to use Variants part 2
+
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 40px;
+  background-color: rgba(255, 255, 255, 0.3);
+`;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  background-color: rgba(255, 255, 255, 0.2);
+  background-color: rgba(255, 255, 255, 0.8);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
   hover: {
-    scale: 2,
     rotateZ: 90,
   },
-  click: { scale: 1, borderRadius: '100px' },
-  drag: { backgroundColor: 'rgb(46,204,113)', transition: { duration: 3 } },
+  click: { borderRadius: '100px' },
 };
 
-// delay children: https://www.framer.com/motion/transition/#orchestration:~:text=%23-,Orchestration,-%23
-
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
+
   return (
     <Wrapper>
-      <Box
-        variants={boxVariants}
-        drag
-        // color를 "blue", "white"처럼 넣으면 애니메이션 적용이 안된다.
-        // 아래처럼 rgb or rgba로 넣자
-        whileDrag="drag"
-        // whileHover={조건 ? "hover":"other"} 조건문도 가능
-        whileHover="hover"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          variants={boxVariants}
+          // limiting drag direction
+          // drag="x"
+          drag
+          // dragConstraints : limiting droppable region
+          // 1. manually decided
+          // dragConstraints={{ top: -200, bottom: 200, left: -200, right: 200 }}
+          // 2. decided by parent node
+          dragConstraints={biggerBoxRef}
+          // drag 후 처음 위치로 복귀
+          dragSnapToOrigin
+          // dragElastic : 0~1 (0: 영역 밖에서 작용하는 힘 min / 1 : max) 써보면 앎.
+          dragElastic={0.5}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
