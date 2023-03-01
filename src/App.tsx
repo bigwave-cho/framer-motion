1425874ac42ad83a784eab5a10565d3d15f6070a
bigwave-cho <hooyaa93@gmail.com>
2023-03-01 18:21:17 +0900

    framer-motion : variants 기본 적용법

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const Wrapper = styled.div`
   justify-content: center;
   align-items: center;
 `;
-// ## How to use styled-components with framer-motion
+// ## How to use Variants
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
@@ -16,18 +16,19 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const myVars = {
+  start: { scale: 0 },
+  end: { scale: 1, rotateZ: 360, transition: { type: 'spring', delay: 0.5 } },
+};
+
 function App() {
   return (
     <Wrapper>
       <Box
-        //https://www.framer.com/motion/transition/
-        //animate 적용하면 약간 튕기는 듯한 모션이 발생하는데
-        //모든 애니메이션에는 spring이라는 옵션이 default
-        // damping, stifness...
-        transition={{ type: 'spring', delay: 0.5 }}
-        //initial : initial state
-        initial={{ scale: 0 }}
-        animate={{ scale: 1, rotateZ: 360 }}
+        //
+        variants={myVars}
+        initial="start"
+        animate="end"
       />
     </Wrapper>
   );
