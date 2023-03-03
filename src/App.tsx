c59b1a4c4a945c98ba17b7e202a35c38bb6527cd
bigwave-cho <hooyaa93@gmail.com>
2023-03-03 11:53:46 +0900

    framer-motion : sharedLayout

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,24 @@ import styled from 'styled-components';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 /*
-layout : layout 속성을 부여한 요소의 변화를 감지하여
+## layout : layout 속성을 부여한 요소의 변화를 감지하여
 레이아웃이 변할 때마다 자동으로 애니메이션 적용.
 
 자세히는 layout에 커서 올려보면 나옴. 참고하셈
 https://www.framer.com/docs/animate-shared-layout/#syncing-layout-animations
+
+## sharedLayout
+Animate between components
+AnimateSharedLayout은 동일한 layoutId prop을 가진 모션 컴포넌트들 간에 애니메이션을 적용할 수 있습니다. layoutId가 있는 새 컴포넌트가 추가되고 다른 컴포넌트가 제거되면 이전 컴포넌트에서 새 컴포넌트로 레이아웃 애니메이션을 수행합니다. 새 컴포넌트는 이전 컴포넌트의 애니메이션 값도 초기 상태로 상속합니다. 따라서 시각적으로 하나의 연속 컴포넌트로 처리됩니다.
+ex) isSelected && < motion.div layoutId="underline" />
+https://www.framer.com/docs/animate-shared-layout/#animate-between-components
 */
 const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
   background-color: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
 `;
 
@@ -43,13 +48,19 @@ function App() {
 
   return (
     <Wrapper onClick={toggleClicked}>
-      <Box
-        style={{
-          justifyContent: clicked ? 'center' : 'flex-start',
-          alignItems: clicked ? 'center' : 'flex-start',
-        }}
-      >
-        <Circle layout />
+      <Box>
+        {!clicked ? (
+          <Circle
+            // layoutId를 일치시켜주면 framer 가 해당 컴포넌트들을 연결시켜버림..ㄷㄷ
+            layoutId="circle"
+            style={{ borderRadius: '50px' }}
+          />
+        ) : null}
+      </Box>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0 }} />
+        ) : null}
       </Box>
     </Wrapper>
   );
